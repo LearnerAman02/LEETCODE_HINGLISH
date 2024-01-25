2143474ddf32669cf560e324e6d0ed7dbaaa391e
Aman Singh <128976778+Educatoraman@users.noreply.github.com>
2024-01-25 06:28:04 +0530

    Optimal Approach for place non duplicates at the start of the array

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+// problem link --> https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Optimal Approach --> keep a ptr i=0,j=1 and count=0 
+// iterate the array with the help of j ptr and check karo ki kya nums[i]!=nums[j] than just increment karo i index ko and store nums[j] over there and also increment counter
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        int j=1;
+        int count=0;// to store the count of not duplicate elements
+        while(j<nums.length){
+            if(nums[i]!=nums[j]){
+                i++;
+                count++;
+                nums[i]=nums[j];
+            }
+            j++;
+        }
+        return count+1;// because count 0 se start hua tha
+    }
+}
